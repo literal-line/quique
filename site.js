cf6388891b6028faee2d620fcb8eaac2248d6c0b
Literal Line <>
2021-01-26 08:54:02 -0500

    better scaling

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site = (function () { // crap code but i was in a hurry
     var iframe = document.createElement('iframe');
-    iframe.style = 'width: 100%; border: none; width: 99vw;';
+    iframe.style = 'width: 100%; border: none; width: 95vw;';
     document.body.appendChild(iframe);
     var jsNotice = document.getElementById('jsNotice');
     jsNotice.remove();
@@ -19,19 +19,19 @@ var site = (function () { // crap code but i was in a hurry
 
     var urlChange = function () {
         var pathname = iframe.contentWindow.location.pathname.slice(1);
-        window.location.hash = pathname === 'home.html' ? '' : pathname;
+        window.location.hash = pathname === 'home' ? '' : pathname;
     };
 
     iframe.onload = urlChange;
 
     if (window.location.hash) {
         if (window.location.hash === '#') {
-            iframe.src = 'home.html';
+            iframe.src = 'home';
         } else {
             iframe.src = window.location.hash.slice(1);
         }
     } else {
-        iframe.src = 'home.html';
+        iframe.src = 'home';
     }
 
     console.log('%cQuique.gq', 'background: #304888; border: 1px solid #FFFFFF; padding: 10px; border-radius: 3px; font-family: "Data 70 Regular"; font-size: 72px; color: #FFFFFF; -webkit-text-stroke: 2px #000000;');
